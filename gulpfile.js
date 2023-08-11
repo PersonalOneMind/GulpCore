7f6a4b59f7c1a77d00cee9bcadcc16b6a0480618
Space-Guard <space-wow@mail.ru>
2023-08-11 11:10:33 +0300

    Add styles convers from sass to css

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+// import pkg from 'gulp';
+// const { src, dest } = pkg;
+// import sass from 'gulp-sass';
+// import concat from 'gulp-concat';
+/*Может когда то перепишу под ES6...*/
+
+const { src, dest } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+
+function styles() {
+	return src('./src/style/sass/style.sass')
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(concat('style.min.css'))
+		.pipe(dest('dist/style/style.css'))
+}
+
+exports.styles = styles;
