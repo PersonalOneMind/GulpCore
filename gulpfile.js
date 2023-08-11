657308a152ab583aaaee9958034e97867f0b40c6
Space-Guard <space-wow@mail.ru>
2023-08-11 11:44:26 +0300

    Add BrowserSync and parallel watching

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,35 @@
 // import concat from 'gulp-concat';
 /*Может когда то перепишу под ES6...*/
 
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
 
 function styles() {
 	return src('./src/style/sass/style.sass')
 		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
 		.pipe(concat('style.min.css'))
 		.pipe(dest('dist/style/style.css'))
+		.pipe(browserSync.stream());
 }
 
 function watching() {
-	watch(['./src/style/sass/**/*.sass'], styles)
+	watch(['./src/style/sass/**/*.sass'], styles);
+	watch(['./src/*.pug']).on('change', browserSync.reload);
+}
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: "./src"
+		}
+	});
 }
 
 exports.styles = styles;
 exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.default = parallel(browsersync, watching)
